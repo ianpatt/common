66149ee3e5cc783884658bc9500e944d3a61bfbe
ianpatt <ianpatt@gmail.com>
2021-03-21 21:10:38 -0700

    update required windows header version

--- common/IPrefix.h
+++ common/IPrefix.h
@@ -10,8 +10,8 @@
 // 4312 - pointer extension
 #pragma warning(disable: 4018 4200 4244 4267 4305 4288 4312 4311)
 
-// winxp and above
-#define _WIN32_WINNT	0x0501
+// need win8 for windows store APIs
+#define _WIN32_WINNT	0x0602
 
 #include <cstdlib>
 #include <cstdio>
